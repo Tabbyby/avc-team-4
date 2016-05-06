3f36b6e67c7be661023609150158c87b98b306c9
softangu <softangu@users.noreply.github.com>
2016-05-06 15:30:54 +1200

    add indenting

--- PID.c
+++ PID.c
@@ -16,32 +16,32 @@ double speedPecent=100/255;
 int SPEED;
 
 int main(){
-//geting the time diference each time throght the loop
-double timeDif = currentTime-time;
-//resets the time
-time=currentTime;
-//sets the prev error (before the loop)
-preverror=error;
-//sets error(after the loop)
-error=sum;
-//sets current error
-currentErro+=error;
-//gets proportional erros signal
-propErrorSignal = error*kp;
-//gets diferential error signal
-difErrorSignal=((error-prevError)/timeDif)*kd;
-//gets the integral error signal
-inteErrorSignal=((currentError)*ki);
-//updated the total error
-errorSum+=currentError;
-//checks what error is bigeer to set the speed
-if(difErrorSignal>propErrorSignal||inteErrorSignal>propErrorSignal){
-if(difErrorSignal>inteErrorSignal){
-SPEED=difErrorsignal*speedPercentage;
-}else{
-SPEED=inteErrorsignal*speedPercentage;
-}
-}else{
-SPEED=255;
-}
+	//geting the time diference each time throght the loop
+	double timeDif = currentTime-time;
+	//resets the time
+	time=currentTime;
+	//sets the prev error (before the loop)
+	preverror=error;
+	//sets error(after the loop)
+	error=sum;
+	//sets current error
+	currentErro+=error;
+	//gets proportional erros signal
+	propErrorSignal = error*kp;
+	//gets diferential error signal
+	difErrorSignal=((error-prevError)/timeDif)*kd;
+	//gets the integral error signal
+	inteErrorSignal=((currentError)*ki);
+	//updated the total error
+	errorSum+=currentError;
+	//checks what error is bigeer to set the speed
+	if(difErrorSignal>propErrorSignal||inteErrorSignal>propErrorSignal){
+		if(difErrorSignal>inteErrorSignal){
+			SPEED=difErrorsignal*speedPercentage;
+		}else{
+			SPEED=inteErrorsignal*speedPercentage;
+		}else{
+			SPEED=255;
+		}
+	}
 }
