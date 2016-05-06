96748f8772e3ba9bd70fc312c39b5053ae29c22c
softangu <softangu@users.noreply.github.com>
2016-05-06 14:57:48 +1200

    Create Network_Gate.cpp

---
+++ Network_Gate.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+
+extern "C" int init(int d_lev);
+//network commands
+extern "C" int connect_to_server( char server_addr[15],int port);
+extern "C" int send_to_server(char message[24]);
+extern "C" int receive_from_server(char message[24]);
+
+Network_Gate(){
+	init(0);
+	connect_to_server(130.195.6.196, 1024);
+	//sends MESSAGE to server
+	send_to_server("Please");
+	//recieves from server
+	char message[24];
+	recieve_from_server(message);
+	//sends password back to server
+	send_to_server(message);
+
+}
