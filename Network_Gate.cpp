44f25984813949f15882430fdafb52a6b53be03c
softangu <softangu@users.noreply.github.com>
2016-05-10 15:27:59 +1200

    fix a few minor bugs
    still doesn't work

--- Network_Gate.cpp
+++ Network_Gate.cpp
@@ -7,17 +7,18 @@ extern "C" int connect_to_server( char server_addr[15],int port);
 extern "C" int send_to_server(char message[24]);
 extern "C" int receive_from_server(char message[24]);
 
-Network_Gate(){
+int main(){
 	//initialises hardware
 	init(0);
 	//connects to server at IP ADDRESS, PORT
-	connect_to_server(130.195.6.196, 1024);
+	connect_to_server("130.195.6.196", 1024);
 	//sends MESSAGE to server
 	send_to_server("Please");
 	//recieves from server
 	char message[24];
-	recieve_from_server(message);
+	receive_from_server(message);
 	//sends password back to server
 	send_to_server(message);
-
+	return 0;
+	
 }
